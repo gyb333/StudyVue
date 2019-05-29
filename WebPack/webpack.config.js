ad5df289f62fec2c77b6b2d2587942443dbe70fa
gyb333 <876368840@qq.com>
2019-05-29 17:39:56 +0800

    test

--- WebPack/webpack.config.js
+++ WebPack/webpack.config.js
@@ -10,23 +10,30 @@ module: {
      rules: [
        {
          test: /\.css$/, //打包css文件
-         use: [
-           'style-loader',
-           'css-loader'
-         ],
+         use: ['style-loader','css-loader'],
         // loader:'style-loader!css-loader'
        },
        {
         test: /\.scss$/,  //打包scss文件
         // loader: ExtractTextPlugin.extract("style", 'css!sass') //这里用了样式分离出来的插件，如果不想分离出来，可以直接这样写 
          loader:'style-loader!css-loader!sass-loader'
-      }
-      ,
+      },
        {
         test: /\.less$/,  //打包scss文件
         // loader: ExtractTextPlugin.extract("style", 'css!sass') //这里用了样式分离出来的插件，如果不想分离出来，可以直接这样写 
          loader:'style-loader!css-loader!sass-loader!less-loader'
-      }
+      }, 
+      {  
+        test: /\.(jpeg|jpg|gif|png)$/,  
+        use: [  
+            {  
+                loader: 'url-loader',  
+                options: {  
+                    limit: '102400' //小于100k 打包到build.js里面
+                }  
+            },  
+        ]  
+    }  
      ]
    }
 }
